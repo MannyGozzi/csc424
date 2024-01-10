600169a6c2350df2a8ab80005b1307ef4e26fa25
Noctua <mannygozzidev@protonmail.com>
2024-01-09 21:33:57 -0800

    Clear field on bad registartion

--- react-frontend/src/Home.tsx
+++ react-frontend/src/Home.tsx
@@ -1,56 +1,72 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useAuth } from "./context/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
-
-const Home = () => { 
+const Home = () => {
   const value = useAuth();
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
-  const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const passwordField = useRef<HTMLInputElement>(null);
 
   const onSignIn = async () => {
-  value?.onLogin(username, password).then((success) => {
-    if (success) {
-      setError("")
-      navigate("/landing")
-    } else {
-      setError("Login failed")
-    }
-  })   
-  }
+    value?.onLogin(username, password).then((success) => {
+      if (success) {
+        setError("");
+        navigate("/landing");
+      } else {
+        setError("Login failed");
+      }
+    });
+  };
 
   const onRegister = async () => {
     value?.onRegister(username, password).then((success) => {
       if (success) {
-        setError("")
-        navigate("/landing")
+        setError("");
+        navigate("/landing");
       } else {
-        setError("Registration failed")
+        setError("Registration failed, 8+ chars, 1 num, 1 sym.");
+        if (passwordField.current) passwordField.current.value = "";
       }
-    })   
-  }
+    });
+  };
 
   return (
-    <>
+    <div>
       <h2>Home (Public)</h2>
-      <input 
-          type="text" 
-          placeholder="username" 
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}/>
-      <input 
-          type="password" 
-          placeholder="password" 
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}/>
-      <button type="button" onClick={onSignIn}>
-        Sign In
-      </button>
-      <button type="button" onClick={onRegister}>
-        Register
-      </button>
-      <div>{error}</div>
-  </>);
+      <div className="flexRow">
+        <div className="accountInputsGroup">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUsername(e.target.value)
+            }
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+            ref={passwordField}
+          />
+        </div>
+        <div className="accountButtonsGroup">
+          <button type="button" onClick={onSignIn}>
+            Sign In
+          </button>
+          <button type="button" onClick={onRegister}>
+            Register
+          </button>
+        </div>
+      </div>
+      <div className="errorMsg">{error}</div>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
